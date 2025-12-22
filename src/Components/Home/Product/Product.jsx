d73511cceb32812f673a693afa84c0e3f88f90a4
Shreya-poojari-28 <shreyapoojari28@gmail.com>
2025-12-22 06:40:47 +0530

    commit from poojari

--- src/Components/Home/Product/Product.jsx
+++ src/Components/Home/Product/Product.jsx
@@ -7,7 +7,7 @@ const Product = () => {
             <div className="product-container">
                 <img src="https://cdn.pixabay.com/photo/2023/05/08/21/59/tshirt-7979852_1280.jpg" alt="t-shirt" />
                 <div className="product-details px-2">
-                    <p>Product name</p>
+                    <p>Product name: </p>
                     <p>Rating: 4.5
                         <span className="rating-stars mx-2">
                             <i className="fa-solid fa-star"></i>
