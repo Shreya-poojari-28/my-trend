92510677847ef4604866cbca705adb9e2e74a5f0
Shreya-poojari-28 <shreyapoojari28@gmail.com>
2025-12-25 06:08:45 +0530

    Dark theme for product card

--- src/Components/Home/Product/Product.jsx
+++ src/Components/Home/Product/Product.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Product.css'
+import { ThemeProvider } from '../../../ThemeProvider/ThemeProvider'
 
 const Product = ({
     productId,
@@ -8,10 +9,13 @@ const Product = ({
     rating,
     title
 }) => {
+
+    const themeData = useContext(ThemeProvider)
+    const theme = themeData.theme;
     
     return (
         <div className="product p-2">
-            <div className="product-container">
+            <div className={`product-container p-2 ${theme}`}>
                 <img src={image} alt="t-shirt" />
                 <div className="product-details px-2">
                     <p>Product name</p>
